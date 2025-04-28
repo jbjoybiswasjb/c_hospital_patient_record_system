861bbd3e486364e8cf35e5d80a9b10d1c3edd1bd
Joy Biswas <jbjoybiswasjb@gmail.com>
2025-04-29 03:25:37 +0600

    Printed some options.

---
+++ main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("\n Welcome to our hospital patient record system !!!\n");
+    printf("\n 1. See doctor's list and appointment time \n 2. Create a patient account \n 3. Patient login \n 4. Admin login \n 5. Exit \n");
+    return 0;
+}
